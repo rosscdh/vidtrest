18e946118df5fa1a29308c2150f521a70a973c9a
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2016-04-21 06:34:34 +0200

    added rollbar

--- vidtrest/settings.py
+++ vidtrest/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
 
 ROOT_URLCONF = 'vidtrest.urls'
