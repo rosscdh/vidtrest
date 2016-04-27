c6353f8bd301022c012a573f3cec7737a68bd0e0
Alfonz Borsos <alfonz.borsos@gmail.com>
2016-04-27 10:52:48 +0200

    fixed tooltip

--- vidtrest/apps/vid/static/js/main.js
+++ vidtrest/apps/vid/static/js/main.js
@@ -2,9 +2,16 @@
 
 // copy to Clipboard
 new Clipboard("#copytoclipboard");
-$('#copytoclipboard').tooltip({
+var $copybutton = $('#copytoclipboard').tooltip({
   animation: true,
-  delay: { "show": 500, "hide": 100 }
+  delay: { "show": 200, "hide": 200 },
+  trigger: 'click',
+  placement: 'bottom'
+});
+$copybutton.on('mouseleave', function() {
+  if ($(this).attr('aria-describedby') !== undefined) {
+    $(this).click();
+  }
 });
 
 // jQuery to collapse the navbar on scroll
