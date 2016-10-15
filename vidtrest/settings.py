7bf4947556da5d60d6dcdb760f5e3a67b865d742
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2016-10-15 21:34:00 +0200

    test db

--- vidtrest/settings.py
+++ vidtrest/settings.py
@@ -90,11 +90,21 @@ WSGI_APPLICATION = 'vidtrest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vidtrest_development',
+        'HOST': '192.168.0.103',
+        'PORT': '5432',
+        'USER': 'rosscdh',
+        'PASSWORD': '',
+    },
 }
 
 
