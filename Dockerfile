e3fa4af01c0bb0e9835b0510d76cc169264a9cfe
Ross Crawford-d'Heureuse <sendrossemail@gmail.com>
2017-10-08 20:29:47 +0200

    fixes

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM python:3.6.2-stretch
 
 WORKDIR /
 
+ADD requirements.txt /
 RUN curl -sL https://deb.nodesource.com/setup_8.x | bash -
-RUN npm -g install bower gulp
 RUN apt-get update
-RUN apt-get install -y exiftool ffmpeg
+RUN apt-get install -y exiftool ffmpeg nodejs
+RUN npm install -g bower gulp
 RUN pip install -r /requirements.txt
 
 #CMD ["honcho", "start"]
--- config/production/local_settings.py
+++ config/production/local_settings.py
@@ -23,3 +23,59 @@ EMAIL_HOST_USER = os.getenv('SENDGRID_USER')
 EMAIL_HOST_PASSWORD = os.getenv('SENDGRID_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(process)-5d %(thread)d %(name)-50s %(levelname)-8s %(message)s'
+        },
+        'simple': {
+            'format': '[%(asctime)s] %(name)s %(levelname)s %(message)s',
+            'datefmt': '%d/%b/%Y %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'syslog': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.SysLogHandler',
+            'facility': 'local7',
+            'address': '/dev/log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        # root logger
+        '': {
+            'handlers': ['console', 'syslog'],
+            'level': 'INFO',
+            'disabled': False
+        },
+        'django': {
+            'handlers': ['console', 'syslog'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console', 'syslog'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
